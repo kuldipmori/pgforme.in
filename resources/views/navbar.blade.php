7f061e92af14e1de487d5f28fe12fe1b38885e18
iprajapatiparesh <prajapatiparesh894@gmail.com>
2023-12-31 12:42:48 +0530

    phase1

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,47 @@
+<body>
+    <div class="container-xxl bg-white p-0">
+ 
+
+        <!-- Navbar Start -->
+        <div class="container-fluid nav-bar bg-transparent">
+            <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
+                <a href="index.html" class="navbar-brand d-flex align-items-center text-center">
+                    <div class="icon p-2 me-2">
+                        <img class="img-fluid" src="img/icon-deal.png" alt="Icon" style="width: 30px; height: 30px;">
+                    </div>
+                    <h1 class="m-0 text-primary">PGFORME</h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto">
+                        <a href="#" class="nav-item nav-link active">Home</a>
+                        <a href="#" class="nav-item nav-link">Buy</a>
+                        <a href="#" class="nav-item nav-link">Sell</a>
+                        <a href="#" class="nav-item nav-link">Rent</a>
+                        <a href="#" class="nav-item nav-link">PG</a>
+                        <a href="#" class="nav-item nav-link">Hostel</a>
+                        <a href="#" class="nav-item nav-link">About</a>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Property</a>
+                            <div class="dropdown-menu rounded-0 m-0">
+                                <a href="property-list.html" class="dropdown-item">Property List</a>
+                                <a href="property-type.html" class="dropdown-item">Property Type</a>
+                                <a href="property-agent.html" class="dropdown-item">Property Agent</a>
+                            </div>
+                        </div>
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                            <div class="dropdown-menu rounded-0 m-0">
+                                <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                                <a href="404.html" class="dropdown-item">404 Error</a>
+                            </div>
+                        </div>
+                        <a href="contact.html" class="nav-item nav-link">Contact</a>
+                    </div>
+                    <a href="" class="btn btn-primary px-3 d-none d-lg-flex">Add Property</a>
+                </div>
+            </nav>
+        </div>
+        <!-- Navbar End -->
---
+++ resources/views/search.blade.php
@@ -0,0 +1,63 @@
+<!-- Search Start -->
+<div class="container-fluid bg-primary mb-5 wow fadeIn mt-3" data-wow-delay="0.1s" style="padding: 35px;">
+    <div class="container">
+        <div class="row g-2">
+            <div class="col-md-10">
+                <div class="row g-2">
+                    <div class="col-md-2">
+                        <input type="text" class="form-control border-0 py-3" placeholder="Search Keyword">
+                    </div>
+                    <div class="col-md-2">
+                        <select class="form-select border-0 py-3">
+                            <option selected>Property Type</option>
+                            <option value="1">PG</option>
+                            <option value="2">Hostel</option>
+                            
+                        </select>
+                    </div>
+                    <div class="col-md-2">
+                        <select class="form-select border-0 py-3">
+                            <option selected>Location</option>
+                            <option value="1">Location 1</option>
+                            <option value="2">Location 2</option>
+                            <option value="3">Location 3</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-2">
+                        <select class="form-select border-0 py-3">
+                            <option selected>Gender</option>
+                            <option value="1">Boys</option>
+                            <option value="2">Girls</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-2">
+                        <select class="form-select border-0 py-3">
+                            <option selected>Sharing</option>
+                            <option value="1">1 Sharing</option>
+                            <option value="2">2 Sharing</option>
+                            <option value="1">3 Sharing</option>
+                            <option value="2">4 Sharing</option>
+                            <option value="1">5 Sharing</option>
+                            <option value="2">6 Sharing</option>
+                        </select>
+                    </div>
+
+                    <div class="col-md-2">
+                        <select class="form-select border-0 py-3">
+                            <option selected>Budget</option>
+                            <option value="1">5K to 10k</option>
+                            <option value="2">10K to 15k</option>
+                            <option value="1">15K to 20k</option>
+                        </select>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-dark border-0 w-100 py-3">Search</button>
+            </div>
+        </div>
+    </div>
+</div>
